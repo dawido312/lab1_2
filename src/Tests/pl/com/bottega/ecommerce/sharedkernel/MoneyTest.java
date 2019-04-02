7f1a1e1e6fcd42a7de580cbdee32b45c61717a09
Dawid Blus <dawido312@gmail.com>
2019-04-02 21:29:02 +0200

    Dodanie testu sprawdzającego poprawność działania funkcji mnożącej dla klasy Money.

---
+++ src/Tests/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -0,0 +1,28 @@
+package pl.com.bottega.ecommerce.sharedkernel;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class MoneyTest {
+
+    private Money money, money2, money3;
+
+    @Before public void setUp(){
+        money = new Money(10, "PLN");
+        money2 = new Money(20,"PLN");
+        money3 = new Money(30, "EUR");
+    }
+
+    @Test public void multiplyByTest()
+    {
+        Money tmp = money.multiplyBy(3d);
+        Assert.assertThat("30.00 zł", is(tmp.toString()));
+        tmp.multiplyBy(-3d);
+        Assert.assertThat("30.00 zł", is(tmp.toString()));
+    }
+
+}
